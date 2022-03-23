220043c0faeb48153ad2469b5a2b5b0ec053b5fa
WDev-Beginner <ab.serghine@gmail.com>
2022-03-23 23:25:45 +0100

    added navbar/footer, setup intial structure for components/containers

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Footer.css';
+
+export default function Footer() {
+  return <div className="footer">Copyright © 2022 Interview SS All rights reserved.</div>;
+}
