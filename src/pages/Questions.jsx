f1b1d35a9c4f01f07a9a08ce8fea58e29300b96f
Sasha Verdiyev <sasha.verdiyev@gmail.com>
2022-03-28 23:12:49 +0200

    change folder strucutre

---
+++ src/pages/Questions.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Question from '../components/Question.jsx';
+
+function Questions() {
+  return (
+    <>
+      Questions Page
+      <Question />
+    </>
+  );
+}
+
+export default Questions;
