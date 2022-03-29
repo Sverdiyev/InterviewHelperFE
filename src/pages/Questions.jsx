269130a47a4aa030d489c4cdf7eab44c8d13f276
Sasha Verdiyev <sasha.verdiyev@gmail.com>
2022-03-29 23:16:58 +0200

    added css based question component

--- src/pages/Questions.jsx
+++ src/pages/Questions.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import Question from '../components/Question.jsx';
 
+const questions = [
+  {
+    Id: 1,
+    Complexity: 'easy',
+    QuestionContent: 'asd',
+    Note: 'asdqwe',
+    Vote: 5,
+    Tags: ['tag 1', 'tag 2']
+  }
+];
+
 function Questions() {
   return (
     <>
-      Questions Page
-      <Question />
+      {questions.map((question) => (
+        <Question key={question.Id} {...question} />
+      ))}
     </>
   );
 }
