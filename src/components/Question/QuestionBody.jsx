f71b402e3b8e561adb1436a57b942f71a2d044f7
Sasha Verdiyev <sasha.verdiyev@gmail.com>
2022-03-31 19:56:05 +0200

    refactor QuestionBody component

--- src/components/Question/QuestionBody.jsx
+++ src/components/Question/QuestionBody.jsx
@@ -1,18 +1,33 @@
-import { CardContent } from '@mui/material';
+import { CardContent, Grid } from '@mui/material';
+import { styled } from '@mui/system';
 import React from 'react';
-import classes from './Question.module.css';
+
+const StyledTag = styled('span')({
+  marginRight: '10px',
+  color: 'white',
+  backgroundColor: 'grey',
+  borderRadius: '25px',
+  padding: '3px 7px',
+  fontSize: '0.8rem',
+  whiteSpace: 'nowrap',
+  overflow: 'hidden',
+  maxWidth: '100px'
+});
 
 function QuestionBody({ Note, Tags }) {
   return (
-    <CardContent className={classes.contentText} sx={{ padding: 0 }}>
-      <p className={classes.description}>{Note} </p>
-      <div className={classes.tags}>
+    <CardContent
+      component={Grid}
+      container
+      direction="column"
+      justifyContent="space-between"
+      sx={{ padding: 0, flex: 1 }}>
+      <span style={{ fontSize: '0.8rem' }}>{Note} </span>
+      <Grid alignItems="center" sx={{ width: '80%' }}>
         {Tags.map((tag) => (
-          <span key={tag} className={classes.tag}>
-            {tag[0].toUpperCase() + tag.slice(1)}
-          </span>
+          <StyledTag key={tag}>{tag[0].toUpperCase() + tag.slice(1)}</StyledTag>
         ))}
-      </div>
+      </Grid>
     </CardContent>
   );
 }
