3fd350e0057f24ab36e7a1d779c6a651a6759f7e
Sasha Verdiyev <sasha.verdiyev@gmail.com>
2022-03-31 19:20:58 +0200

    refactor Actions component

--- src/components/Question/Actions.jsx
+++ src/components/Question/Actions.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
-import classes from './Question.module.css';
 import ArrowUp from '@mui/icons-material/ArrowDropUp';
 import ArrowDown from '@mui/icons-material/ArrowDropDown';
-import { Button, CardActions } from '@mui/material';
+import { Button, CardActions, Grid } from '@mui/material';
+import { styled } from '@mui/system';
+
+const StyledCardActions = styled(CardActions)({
+  padding: 0,
+  gap: '5px',
+  '.MuiButton-root': { paddingTop: 0, paddingBottom: 0 }
+});
 
 function Actions({ Vote }) {
   return (
-    <CardActions sx={{ padding: '0' }}>
-      <span className={classes.rating}>{Vote > 0 ? '+' + Vote : Vote}</span>
-
-      <div className={classes.buttons}>
-        <Button size="small" sx={{ paddingTop: 0, paddingBottom: 0 }}>
+    <StyledCardActions>
+      <span>{Vote > 0 ? '+' + Vote : Vote}</span>
+      <Grid container direction="column">
+        <Button size="small">
           <ArrowUp fontSize="large" sx={{ color: '#549f60' }} />
         </Button>
-        <Button size="small" sx={{ paddingTop: 0, paddingBottom: 0 }}>
+        <Button size="small">
           <ArrowDown fontSize="large" sx={{ color: '#d78418' }} />
         </Button>
-      </div>
-    </CardActions>
+      </Grid>
+    </StyledCardActions>
   );
 }
 
