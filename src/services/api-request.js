a4e841bb5310b8ba86e48b38f081c6753e6f4d16
Sasha Verdiyev <sasha.verdiyev@gmail.com>
2022-04-03 18:56:02 +0200

    added BE communication

--- src/services/api-request.js
+++ src/services/api-request.js
@@ -1,4 +1,68 @@
-const baseUrl = '/';
+import { useQuery } from 'react-query';
 
-export const getQuestions = () => {};
-export const postQuestion = (question) => {};
+//my mock api url. The data is not filled in properly, so the UI will look slightly weird
+const baseUrl = 'https://6249c785fd7e30c51c0668f4.mockapi.io/';
+
+//get all questions
+export const getQuestions = () => {
+  const url = baseUrl + 'questions';
+
+  return useQuery(
+    'questions',
+    async () => {
+      const data = await (await fetch(url)).json();
+      return data;
+    },
+    {
+      notifyOnChangeProps: ['data', 'error']
+    }
+  );
+};
+
+// generic get request
+export const getEndpoint = (endpoint) => {
+  const url = baseUrl + endpoint;
+
+  return useQuery(
+    endpoint,
+    async () => {
+      const data = await (await fetch(url)).json();
+      return data;
+    },
+    {
+      notifyOnChangeProps: ['data', 'error']
+    }
+  );
+};
+
+//post new question
+export const postQuestion = async (question) => {
+  const url = baseUrl + 'questions';
+
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(question)
+  });
+
+  const data = await response.json();
+  return data;
+};
+
+//generic post request
+export const postData = async (endpoint, inputData) => {
+  const url = baseUrl + endpoint;
+
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(inputData)
+  });
+
+  const data = await response.json();
+  return data;
+};
