c87eb0cbf784e6ba7c68877b47044e88ef25b6a8
Sasha Verdiyev <sasha.verdiyev@gmail.com>
2022-04-04 12:02:22 +0200

    removed test button

--- src/pages/Questions.jsx
+++ src/pages/Questions.jsx
@@ -1,23 +1,12 @@
 import React from 'react';
 import Question from '../components/Question/Question.jsx';
-import { getQuestions, postQuestion } from '../services/api-request.js';
-
-const question = {
-  Id: 3,
-  Complexity: 'Easy',
-  QuestionContent:
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident voluptatum debitis eius, quibusdam saepe iusto.',
-  Vote: 5,
-  Tags: ['tag 1', 'tag 2']
-};
+import { getQuestions } from '../services/api-request.js';
 
 function Questions() {
   const { data, error, isFetching } = getQuestions();
 
   return (
     <>
-      {/* Added button below just to show the POST request functionality. Should be deleted before merge  */}
-      <button onClick={() => postQuestion(question)}>Post question - example</button>
       {isFetching && <div>Loading</div>}
       {error && <div>error</div>}
       {!isFetching &&
