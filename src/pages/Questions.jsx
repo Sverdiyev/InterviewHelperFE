424c885ac2baf84cdf6a014a460c8b070a20357f
Sasha Verdiyev <sasha.verdiyev@gmail.com>
2022-04-06 11:56:09 +0200

    changed hook naming

--- src/pages/Questions.jsx
+++ src/pages/Questions.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Question from '../components/Question/Question.jsx';
-import { useGetQuestions } from '../services/api-requests/questions.js';
+import { useQuestions } from '../services/api-requests/questions.js';
 
 function Questions() {
-  const { data, error, isSuccess, isLoading } = useGetQuestions();
+  const { data, error, isSuccess, isLoading } = useQuestions();
 
   return (
     <>
