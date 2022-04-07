4dc26196008293536fee8a3c13d9c1c55331d342
Sasha Verdiyev <sasha.verdiyev@gmail.com>
2022-04-07 20:32:30 +0200

    refactor

--- src/pages/Questions.jsx
+++ src/pages/Questions.jsx
@@ -1,28 +1,29 @@
 import { LinearProgress } from '@mui/material';
 import React from 'react';
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 import Question from '../components/Question/Question.jsx';
 import Search from '../components/Search.jsx';
 import { useQuestions } from '../services/api-requests/questions.js';
 
 function Questions() {
-  const [searchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
   const searchValue = searchParams.get('search');
   const { data, error, isSuccess, isLoading } = useQuestions(searchValue);
 
-  const navigate = useNavigate();
+  const setSearchValue = (value) => setSearchParams(`${value ? '?search=' + value : ''}`);
 
-  const setSearchValue = (value) =>
-    navigate({ pathname: '/questions', search: `${value ? '?search=' + value : ''}` });
   return (
     <>
       <Search setSearchValue={setSearchValue} />
       {isLoading && (
-        <LinearProgress
-          sx={{ width: '36%', height: '7px', margin: '0 auto 0 20%', borderRadius: '25px' }}
-        />
+        <>
+          <LinearProgress
+            sx={{ width: '36%', height: '7px', margin: '0 auto 0 20%', borderRadius: '25px' }}
+          />
+          <div>Loading</div>
+        </>
       )}
-      {isLoading && <div>Loading</div>}
+
       {error && <div>error</div>}
       {isSuccess && data.map((question) => <Question key={question.Id} {...question} />)}
       {isSuccess && data.length === 0 && <div>No Questions Found</div>}
