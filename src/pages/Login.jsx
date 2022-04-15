9b395138a5c08f9b0ea3a4e9f012393354a2ab8a
Sasha Verdiyev <sasha.verdiyev@gmail.com>
2022-04-15 17:24:20 +0200

    login page with validation

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,110 @@
-import React from 'react';
+import { Avatar, Box, Button, Container, CssBaseline, TextField, Typography } from '@mui/material';
+import { LockOutlined as LockOutlinedIcon } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function Login() {
-  return <div>Login</div>;
+  const [emailValue, setEmailValue] = useState('');
+  const [passwordValue, setPasswordValue] = useState('');
+
+  const [emailIsValid, setEmailIsValid] = useState(false);
+  const [passwordIsValid, setPasswordIsValid] = useState(false);
+
+  const [emailIsTouched, setEmailIsTouched] = useState(false);
+  const [passwordIsTouched, setPasswordIsTouched] = useState(false);
+
+  const formIsValid = emailIsValid && passwordIsValid;
+
+  const onChangeEmail = (e) => {
+    setEmailIsTouched(true);
+
+    const value = e.target.value;
+    setEmailValue(value);
+    if (!value.length) return setEmailIsValid(false);
+    setEmailIsValid(true);
+  };
+  const onChangePassword = (e) => {
+    setPasswordIsTouched(true);
+
+    const value = e.target.value;
+    setPasswordValue(value);
+    if (!value.length) return setPasswordIsValid(false);
+    setPasswordIsValid(true);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setEmailIsTouched(true);
+    setPasswordIsTouched(true);
+
+    if (!formIsValid) return;
+
+    const data = { email: emailValue, password: passwordValue };
+
+    console.log('data:', data);
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center'
+        }}>
+        <Avatar sx={{ m: 1, backgroundColor: '#bbb' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={emailValue}
+            onChange={onChangeEmail}
+          />
+          {emailIsTouched && !emailIsValid && 'PLEASE CORRECT email'}
+
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={passwordValue}
+            onChange={onChangePassword}
+          />
+          {passwordIsTouched && !passwordIsValid && 'PLEASE CORRECT PASSWORD'}
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            disabled={passwordIsTouched && emailIsTouched && !formIsValid}
+            sx={{ mt: 3, mb: 2, backgroundColor: '#545454' }}>
+            Sign In
+          </Button>
+
+          <Typography variant="body2" component={Link} to="/signup">
+            Don`t have an account? Sign Up
+          </Typography>
+        </Box>
+      </Box>
+    </Container>
+  );
 }
 
 export default Login;
