59c4ed7b76b913dec9e2de7b499fa4aad650f4c5
Sasha Verdiyev <sasha.verdiyev@gmail.com>
2022-04-22 22:11:23 +0200

    WIP - submit add question

--- src/components/AddQuestion/AddQuestionComponent.jsx
+++ src/components/AddQuestion/AddQuestionComponent.jsx
@@ -1,17 +1,44 @@
 import { Avatar, Checkbox, FormControlLabel, Typography } from '@mui/material';
-import React from 'react';
+import React, { useRef } from 'react';
 import useInputField from '../../services/helpers.js';
 import InputField from '../Login/InputField.jsx';
 import AddIcon from '@mui/icons-material/AddCircleOutline';
 import SubmitButton from '../StyledUI/SubmitButton.jsx';
+import { questionHeadingValidaton } from '../../services/validators.js';
 
 function AddQuestionComponent() {
-  const [headingValue, setHeadingValue, headingIsValid, headingIsTouched] = useInputField(
-    () => true
-  );
+  const [headingValue, setHeadingValue, headingIsValid, headingIsTouched] =
+    useInputField(questionHeadingValidaton);
   const [noteValue, setNoteValue, noteIsValid, noteIsTouched] = useInputField(() => true);
   const [tagsValue, setTagsValue, tagsIsValid, tagsIsTouched] = useInputField(() => true);
 
+  const easyToGoogleValue = useRef(true);
+  const formIsValid = headingIsValid;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!formIsValid) {
+      setHeadingValue();
+      setNoteValue();
+      setTagsValue();
+      return console.log('invalid form');
+    }
+
+    const data = {
+      questionContent: headingValue,
+      Note: noteValue,
+      easyToGoogle: easyToGoogleValue,
+      tags: tagsValue.split(',').map((tag) => tag.trim())
+      //, complexity:...
+    };
+
+    console.log(data);
+
+    //send data to BE
+  };
+
+  //TODO Add complexity selection
   return (
     <>
       <Avatar sx={{ m: 1, backgroundColor: '#a0a0a0' }}>
@@ -20,33 +47,37 @@ function AddQuestionComponent() {
       <Typography component="h1" variant="h5">
         Add Question
       </Typography>
-      <InputField
-        label="Heading"
-        id="heading"
-        inputValue={headingValue}
-        onInputChange={setHeadingValue}
-        autoFocus
-        error={headingIsTouched && !headingIsValid}
-      />
-      <InputField
-        id="note"
-        label="Note"
-        inputValue={noteValue}
-        onInputChange={setNoteValue}
-        error={noteIsTouched && !noteIsValid}
-      />
-      <InputField
-        id="tags"
-        label="Tags"
-        inputValue={tagsValue}
-        onInputChange={setTagsValue}
-        error={tagsIsTouched && !tagsIsValid}
-      />
-      <FormControlLabel
-        control={<Checkbox onChange={(e) => console.log(e.target.checked)} />}
-        label="Easy to google"
-      />
-      <SubmitButton>Add Question</SubmitButton>
+      <form onSubmit={handleSubmit}>
+        <InputField
+          id="heading"
+          label="Heading"
+          inputValue={headingValue}
+          onInputChange={setHeadingValue}
+          autoFocus
+          error={headingIsTouched && !headingIsValid}
+        />
+        <InputField
+          id="note"
+          label="Note"
+          inputValue={noteValue}
+          onInputChange={setNoteValue}
+          error={noteIsTouched && !noteIsValid}
+        />
+        <InputField
+          id="tags"
+          label="Tags - comma-separated"
+          inputValue={tagsValue}
+          onInputChange={setTagsValue}
+          error={tagsIsTouched && !tagsIsValid}
+        />
+        <FormControlLabel
+          control={
+            <Checkbox onChange={(e) => (easyToGoogleValue.current.value = e.target.checked)} />
+          }
+          label="Easy to google"
+        />
+        <SubmitButton disabled={headingIsTouched && !headingIsValid}>Add Question</SubmitButton>
+      </form>
     </>
   );
 }
