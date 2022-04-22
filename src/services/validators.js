e8a4d627d61df65a3968a357e91008fe99678067
Sasha Verdiyev <sasha.verdiyev@gmail.com>
2022-04-22 22:11:01 +0200

    rename validators, add heading validator

--- src/services/validators.js
+++ src/services/validators.js
@@ -1,10 +1,13 @@
-export const emailValidation = (email) => {
+export const emailValidation = (value) => {
   const emailRegExp = new RegExp('^(.+)@(.+)$');
-  return emailRegExp.test(email);
+  return emailRegExp.test(value);
 };
 
 export const passwordValidation = (value) => !!value.length;
-export const nameValidation = (name) => {
+
+export const nameValidation = (value) => {
   const nameRegEx = new RegExp('[1-9]');
-  return !nameRegEx.test(name);
+  return !nameRegEx.test(value);
 };
+
+export const questionHeadingValidaton = (value) => !!value.trim().length;
