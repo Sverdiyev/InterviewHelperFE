6500278338a45d92b86ccc202765e320bd3a1489
Sasha Verdiyev <sasha.verdiyev@gmail.com>
2022-05-03 21:45:27 +0200

    fix view

--- src/components/StyledUI/MultipleSelectField.jsx
+++ src/components/StyledUI/MultipleSelectField.jsx
@@ -2,27 +2,25 @@ import { Checkbox, FormControl, InputLabel, ListItemText, MenuItem, Select } fro
 
 function MultipleSelectField({ values, setValue, options, label }) {
   return (
-    <div>
-      <FormControl sx={{ width: '50%', mt: 1 }}>
-        <InputLabel id={label + '-label'}>{label[0].toUpperCase() + label.slice(1)}</InputLabel>
-        <Select
-          sx={{ textAlign: 'left' }}
-          labelId={label + '-label'}
-          label={label}
-          id="demo-multiple-checkbox"
-          multiple
-          value={values}
-          onChange={(e) => setValue(e)}
-          renderValue={(selected) => selected.join(', ')}>
-          {options.map((option) => (
-            <MenuItem key={option} value={option}>
-              <Checkbox checked={values.includes(option)} />
-              <ListItemText primary={option} />
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    </div>
+    <FormControl fullWidth sx={{ mt: 1 }}>
+      <InputLabel id={label + '-label'}>{label[0].toUpperCase() + label.slice(1)}</InputLabel>
+      <Select
+        sx={{ textAlign: 'left' }}
+        labelId={label + '-label'}
+        label={label}
+        id="demo-multiple-checkbox"
+        multiple
+        value={values}
+        onChange={(e) => setValue(e)}
+        renderValue={(selected) => selected.join(', ')}>
+        {options.map((option) => (
+          <MenuItem key={option} value={option}>
+            <Checkbox checked={values.includes(option)} />
+            <ListItemText primary={option} />
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
   );
 }
 
