8d78d2025d3a7c08c5b221bab2ed254b3a306146
Sasha Verdiyev <sasha.verdiyev@gmail.com>
2022-05-03 21:31:30 +0200

    add multiple select to advanced search

--- src/components/Search/AdvancedSearch.jsx
+++ src/components/Search/AdvancedSearch.jsx
@@ -1,25 +1,24 @@
-import { FormControlLabel, Grid, Radio, RadioGroup } from '@mui/material';
-import { useState } from 'react';
+import { Checkbox, FormControlLabel, Grid } from '@mui/material';
+import { useRef } from 'react';
 import useInputField from '../../services/useInputField.js';
-import InputField from '../StyledUI/InputField.jsx';
+import MultipleSelectField from '../StyledUI/MultipleSelectField.jsx';
 import SelectField from '../StyledUI/SelectField.jsx';
 import SubmitButton from '../StyledUI/SubmitButton.jsx';
 
 function AdvancedSearch() {
-  const [headingValue, setHeadingValue, headingIsValid] = useInputField();
-  const [noteValue, setNoteValue] = useInputField();
-  const [tagsValue, setTagsValue] = useInputField();
   const [complexityValue, setComplexityValue] = useInputField({ defaultValue: 'Not Relevant' });
-  const [easyToGoogle, setEasyToGoogle] = useState('Not Relevant');
+  const hardToGoogleRef = useRef(true);
+  const [tagsValue, setTagsValue] = useInputField({ defaultValue: [] });
+
+  //get all tags
+  const allTags = ['tag1', 'tag2', 'tag3', 'tag4'];
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     const data = {
-      questionContent: headingValue,
-      note: noteValue,
-      easyToGoogle: easyToGoogle,
-      tags: tagsValue.split(',').map((tag) => tag.trim()),
+      hardToGoogle: hardToGoogleRef.current,
+      // tags: tagsValue.split(',').map((tag) => tag.trim()),
       complexity: complexityValue
     };
     console.log('🚀 ~ handleSubmit ~ data', data);
@@ -27,24 +26,14 @@ function AdvancedSearch() {
     // setSuccessfullAddition(postQuestion(data));
     //send data to BE
   };
+
   return (
     <form onSubmit={handleSubmit} noValidate style={{ width: '100%' }}>
-      <InputField
-        id="heading"
-        label="Heading"
-        inputValue={headingValue}
-        onInputChange={setHeadingValue}
-        autoFocus
-        error={headingIsValid === false}
-        multiline
-        rows={2}
-      />
-      <InputField id="note" label="Note" inputValue={noteValue} onInputChange={setNoteValue} />
-      <InputField
-        id="tags"
-        label="Tags - comma-separated"
-        inputValue={tagsValue}
-        onInputChange={setTagsValue}
+      <MultipleSelectField
+        values={tagsValue}
+        setValue={setTagsValue}
+        label="Tags"
+        options={allTags}
       />
       <Grid container alignItems="center">
         <SelectField
@@ -53,13 +42,16 @@ function AdvancedSearch() {
           label="complexity"
           options={['Not Relevant', 'easy', 'medium', 'hard']}
         />
-        <RadioGroup value={easyToGoogle} onChange={(e) => setEasyToGoogle(e.target.value)}>
-          <Grid container>
-            <FormControlLabel value="Not Relevant" control={<Radio />} label="Not Relevant" />
-            <FormControlLabel value={true} control={<Radio />} label="Easy to Google" />
-            <FormControlLabel value={false} control={<Radio />} label="Hard to Google" />
-          </Grid>
-        </RadioGroup>
+        <FormControlLabel
+          sx={{ margin: '0 auto' }}
+          control={
+            <Checkbox
+              defaultChecked
+              onChange={(e) => (hardToGoogleRef.current = e.target.checked)}
+            />
+          }
+          label="Hard to google"
+        />
       </Grid>
       <SubmitButton>Search by Paramaters</SubmitButton>
     </form>
