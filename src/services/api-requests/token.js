2414ead2f6288fbe5db153f7db8cd85f4925f48f
WDev-Beginner <ab.serghine@gmail.com>
2022-05-05 04:59:09 +0200

    added token file, for api testing, log in feature not availale yet

--- src/services/api-requests/token.js
+++ src/services/api-requests/token.js
@@ -1,5 +1,4 @@
 // temp token, generate in swagger for the desired user you wanna test
-const token =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAdGVzdCIsImV4cCI6MTY1MTcxOTA0Nn0.2DAPNIOJh74XFIFlI-All69opwuhTaGMhij9P6AU3YI';
+const token = '';
 
 export default token;
