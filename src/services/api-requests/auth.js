518adfc6ff9f555728cd0cf7c909ef5aee7a25f2
Sasha Verdiyev <sasha.verdiyev@gmail.com>
2022-05-08 21:17:40 +0200

    add user+ login

--- src/services/api-requests/auth.js
+++ src/services/api-requests/auth.js
@@ -24,5 +24,15 @@ export const logOutRequest = () => {
 
 export const addUserRequest = async ({ firstName, email, password }) => {
   //! add lastName support for BE
-  await requestData('/user/add', 'POST', { name: firstName, email, password });
+  const res = await requestData('/user/add', 'POST', { name: firstName, email, password });
+  if (res.ok) {
+    const data = await res.json();
+    const user = { firstName: data.name, lastName: 'Verdiyev' };
+
+    Cookies.set('jwt', data.token);
+    Cookies.set('user', JSON.stringify(user));
+
+    return user;
+  }
+  return false;
 };
