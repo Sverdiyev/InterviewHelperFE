4690a0bc8ece999e7fc3627e3415be790ca921dd
WDev-Beginner <ab.serghine@gmail.com>
2022-05-13 19:45:12 +0200

    removed uneeded user id

--- src/components/Question/QuestionActions.jsx
+++ src/components/Question/QuestionActions.jsx
@@ -49,7 +49,7 @@ function QuestionActions({ questionVote, userVote, questionId }) {
 
   const handleVote = (value) => {
     // insert logged in user id and here
-    const data = { userId: 1, questionId };
+    const data = { questionId };
     if (currentUserVote == value) {
       deleteMutation.mutate(data);
     } else if (value == 'up') {
