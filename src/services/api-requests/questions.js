319d500950236a72f225b43babdb489edee971c9
Oleksandr Verdiyev <sasha.verdiyev@gmail.com>
2022-05-15 21:47:09 +0200

    fix links

--- src/services/api-requests/questions.js
+++ src/services/api-requests/questions.js
@@ -3,11 +3,11 @@ import { getEndpoint, requestData } from './http-client.js';
 //get all questions
 export const useQuestions = (query) => {
   const url = `/questions${query ? '?search=' + query : ''}  `;
-  return getEndpoint(url);
+  return getEndpoint('questions', url);
 };
 
 //post new question
-export const postQuestion = async (question) => requestData('/questions', 'POST', question);
+export const postQuestion = async (question) => requestData('/questions/add', 'POST', question);
 
 //put - edit question
-export const putQuestion = async (question) => requestData('/questions', 'PUT', question);
+export const putQuestion = async (question) => requestData('/questions/edit', 'PUT', question);
