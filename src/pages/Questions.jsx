9196d6d26126d270f356fa7c3d9685bb7e4750c8
WDev-Beginner <ab.serghine@gmail.com>
2022-05-19 22:09:56 +0200

    added side comment column

--- src/pages/Questions.jsx
+++ src/pages/Questions.jsx
@@ -4,7 +4,6 @@ import { useSearchParams } from 'react-router-dom';
 import AddQuestionPopup from '../components/AddQuestion/AddQuestionPopup.jsx';
 import FloatingAddQuestions from '../components/AddQuestion/FloatingAddQuestions.jsx';
 import Question from '../components/Question/Question.jsx';
-import QuestionComments from '../components/QuestionComments/QuestionComments.jsx';
 import Search from '../components/Search/Search.jsx';
 import { useQuestions } from '../services/api-requests/questions.js';
 import { decodeQueryParams } from '../services/helpers.js';
@@ -14,38 +13,39 @@ function Questions() {
 
   const [popupIsVisible, setPopupIsVisible] = useState(false);
   const [searchValues, setSearchValues] = useState(decodeQueryParams(searchParams));
-  const [drawerOpen, setDrawerOpen] = useState(false);
-  const [comments, setComments] = useState(null);
 
   const { data, error, isSuccess, isLoading } = useQuestions(searchValues);
 
   return (
     <>
-      <AddQuestionPopup popupIsVisible={popupIsVisible} setPopupIsVisible={setPopupIsVisible} />
-      <Search searchValues={searchValues} setSearchValues={setSearchValues} />
-      {isLoading && (
-        <Grid container alignItems="center" flexGrow="1" sx={{ paddingBottom: '5%', width: '15%' }}>
-          <CircularProgress size="30%" />
+      <Grid container sx={{ width: '80%', marginLeft: 'auto' }}>
+        <Grid item sx={{ width: '70%' }}>
+          <AddQuestionPopup popupIsVisible={popupIsVisible} setPopupIsVisible={setPopupIsVisible} />
+          <Search searchValues={searchValues} setSearchValues={setSearchValues} />
+          {isLoading && (
+            <Grid
+              container
+              alignItems="center"
+              flexGrow="1"
+              sx={{ paddingBottom: '5%', width: '15%' }}>
+              <CircularProgress size="30%" />
+            </Grid>
+          )}
+          {error && <div>error</div>}
+          {isSuccess && data.map((question) => <Question key={question.id} {...question} />)}
+          {isSuccess && data.length === 0 && <div>No Questions Found</div>}
         </Grid>
-      )}
-      <div style={{ width: '60%' }}>
-        {error && <div>error</div>}
-        {isSuccess &&
-          data.map((question) => (
-            <Question
-              key={question.id}
-              {...question}
-              setDrawerOpen={setDrawerOpen}
-              setComments={setComments}
-            />
-          ))}
-        {isSuccess && data.length === 0 && <div>No Questions Found</div>}
-        <QuestionComments
-          drawerOpen={drawerOpen}
-          setDrawerOpen={setDrawerOpen}
-          comments={comments}
-        />
-      </div>
+        <Grid
+          item
+          container
+          justifyContent="space-between"
+          flexDirection="column"
+          sx={{ width: '30%', marginBottom: '1rem' }}>
+          <Grid sx={{ backgroundColor: 'pink', height: '50vh', position: 'sticky', top: 0 }}>
+            Comments
+          </Grid>
+        </Grid>
+      </Grid>
       <FloatingAddQuestions setPopupIsVisible={setPopupIsVisible} />
     </>
   );
