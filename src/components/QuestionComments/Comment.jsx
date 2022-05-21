65d8df18bf88d496431ca9e0bf7dc4faf3b46e14
Oleksandr Verdiyev <sasha.verdiyev@gmail.com>
2022-05-22 01:06:07 +0200

    added comment date

--- src/components/QuestionComments/Comment.jsx
+++ src/components/QuestionComments/Comment.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import { Avatar, ListItem, ListItemAvatar, ListItemText } from '@mui/material';
 
-export default function Comment({ commentContent, userName }) {
+export default function Comment({ commentContent, userName, creationDate }) {
   const formattedUserName = userName.charAt(0).toUpperCase() + userName.slice(1);
+
+  const parsedDate = new Date(Date.parse(creationDate));
+
+  const creationMonth = parsedDate.toLocaleString('default', { month: 'long' });
+  const creationDay = parsedDate.getDay();
+
   return (
     <>
       <ListItem alignItems="flex-start">
         <ListItemAvatar>
           <Avatar alt={formattedUserName} src="/mock/1.jpg" />
         </ListItemAvatar>
-        <ListItemText primary={formattedUserName} secondary={<>{commentContent}</>} />
+        <ListItemText
+          primary={`${formattedUserName} | ${creationDay}-${creationMonth}`}
+          secondary={<>{commentContent}</>}
+        />
       </ListItem>
     </>
   );
